366b2dd79e6784faa0cdc857f81aa402106b4a83
ranaahammed012 <98573219+ranaahammed012@users.noreply.github.com>
2023-01-27 15:21:47 +0600

    bug fixed 2

--- backend/app.js
+++ backend/app.js
@@ -34,10 +34,12 @@ app.use("/api/v1", payment);
 if (process.env.NODE_ENV == "production") {
 	const path = require("path");
 
-	app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+	app.use(express.static(path.resolve(__dirname, "frontend", "build")));
 
 	app.get("/", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+		res.sendFile(
+			path.resolve(__dirname, "frontend", "build", "index.html")
+		);
 	});
 }
 
